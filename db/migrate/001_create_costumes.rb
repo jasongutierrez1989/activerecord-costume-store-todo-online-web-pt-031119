faee191df1838a90ccada4ef94f040bd391a23eb
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-06-06 04:18:31 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -2,7 +2,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
     create_table :costumes do |t|
       t.string :name
-      t.price :integer
+      t.float :price
       t.string :size
       t.string :image_url
       t.timestamps
