18be96b506fcca60460a05927f433e7438b82f92
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-06-06 04:16:14 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,14 @@
-# Create your haunted_houses migration here
+class HauntedHouse < ActiveRecord::Migration[4.2]
+  class change
+    create_table :hanuted_house do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.integer :price
+      t.boolean :family_friendly
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.string :long_description
+    end
+  end
+end
